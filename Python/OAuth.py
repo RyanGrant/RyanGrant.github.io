081a28c8ca9da54bd8b156f09db1abf86ac7652a
Ryan Grant WLU <grantr16@mail.wlu.edu>
2018-01-07 20:14:09 -0500

    Committing attempt from a couple weeks ago, never found out why JSON was not received properly by API

--- Python/OAuth.py
+++ Python/OAuth.py
@@ -1,17 +1,42 @@
+import requests
+import json
+
+callbackCode = 'ec77fc21a263eb1c9b4ad5b564769014'
+
+headers = {
+    'X-API-Key': '1b3f55b792004e41b894bd035ab93b3d',
+    'Content-Type': 'application/x-www-form-urlencoded'
+}
+payload = {
+    'client_id': '22434',
+    'grant_type': 'authorization_code',
+    'code': callbackCode
+}
+# Adding empty header as parameters are being sent in payload
+headers = {}
+
+r = requests.post(
+    url='https://www.bungie.net/Platform/App/OAuth/Token/',
+    headers=headers,
+    json=payload
+)
+
+# Committing this even though it doesn't work.
+# There's something wrong with the JSON being sent
+#print(r.status_code, r.reason)
+#print(r.text)
+#print(r.content)
 '''
-ReadDestinyInventoryAndVault
-
-GET https://www.bungie.net/en/oauth/authorize?client_id=12345&response_type=code&state=6i0mkLx79Hp91nzWVeHrzHG4
-
-POST https://www.bungie.net/platform/app/oauth/token/ HTTP/1.1
-Authorization: Basic czZCaGRSa3F0MzpnWDFmQmF0M2JW
-Content-Type: application/x-www-form-urlencoded
-
-grant_type=authorization_code&code=SplxlOBeZQQYbYS6WxSbIA
+	var req = {
+		method: 'POST',
+		url: 'https://www.bungie.net/Platform/App/OAuth/Token/',
+		headers: { 
+			'Content-Type': 'application/x-www-form-urlencoded' 
+		},
+		data: { 
+			client_id: '22434',
+			grant_type: 'authorization_code',
+			code: callbackCode
+		}
+	}
 '''
-
-manifest_url = 'https://www.bungie.net/en/oauth/authorize?client_id=12345&response_type=code&state=6i0mkLx79Hp91nzWVeHrzHG4'
-#get the manifest location from the json
-r = requests.get(manifest_url)
-manifest = r.json()
-mani_url = 'http://www.bungie.net'+manifest['Response']['mobileWorldContentPaths']['en']
